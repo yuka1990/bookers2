6ad185e501ba5c130dc168c3624f1803588917ce
YukaTaguchi <164609193+yuka1990@users.noreply.github.com>
2024-04-09 07:06:48 +0000

    [update]bfdeletenew

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,44 @@
 class BooksController < ApplicationController
-  def new
-  end
+  
 
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(list_params)
+    if @book.save
+    redirect_to '/books'
+    else
+    @books=Book.all
+    render :new
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(list_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book= Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  
+  private
+  
+  def list_params
+    params.require(:book).permit(:title, :body)
   end
 end
